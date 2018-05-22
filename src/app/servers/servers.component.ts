f48523d38c58468650f1e3771ca34f28c20f003d
sammy.ha <sammy.ha@kingfisher.com>
2018-05-22 11:59:13 +0100

    third exercise, toggling buttons

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,18 +6,29 @@ import { Component, OnInit } from '@angular/core';
   //           <app-server></app-server>
   //           <app-server></app-server>`,
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
+  styles: [`
+  .logs {
+    color: red;
+  }
+  `]
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = 'Test name';
   servers = ['Test 1', 'Test 2'];
+  noPassword = false;
+  timestamp = ['Tue May 22 2018 11:38:05 GMT+0100'];
 
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
     }, 2000);
+
+    // setTimeout(() => {
+    //   this.noPassword = false;
+    // }, 2000);
   }
 
   ngOnInit() {
@@ -29,6 +40,14 @@ export class ServersComponent implements OnInit {
   }
   newServerName(event: Event) {
     this.serverName = (<HTMLInputElement>event.target).value;
+  }
+  displayDetail() {
+    this.noPassword = true;
+    this.timestamp.push(Date());
+    console.log(this.timestamp);
+    setTimeout(() => {
+      this.noPassword = false;
+    }, 2000);
   }
 
 }
